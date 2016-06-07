70eca3eb802e91cf6464cd60d6e3819d37f0b9b1
KScherer <kirsten.scherer@gmail.com>
2016-06-07 17:46:12 +0200

    first save

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    #Computing the inverse of a square matrix can be done with the solve 
+    #function in R. For example, if X is a square invertible matrix, 
+    #then solve(X) returns its inverse.
+    #For this assignment, assume that the matrix supplied is always invertible.
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #invmat <- x$getinversemat()
+    #if(!is.null(invmat)){
+        #message("getting cached data")
+        #return(invmat)
+    }
+    
+    ## Return a matrix that is the inverse of 'x'
 }
